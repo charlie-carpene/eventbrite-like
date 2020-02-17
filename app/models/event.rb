5ef575a28daafc55ebae9eb1901229aa0338c522
Charlie Carpene <solunacisv@gmail.com>
2020-02-17 17:37:18 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,30 @@
+class Event < ApplicationRecord
+  has_many :attendances
+
+  validates :start_date,
+    presence: true
+
+  validates :duration,
+    presence: true,
+    numericality: true,
+    numericality: { greater_than: 0 }
+
+  validates  :title,
+    presence: true,
+    length: { in: 5..140 }
+
+  validates :description,
+    presence: true,
+    length: { in: 20..1000 }
+
+  validates :price,
+    presence:  true,
+    numericality: true,
+    numericality: {
+      greater_than: 1,
+      less_than: 1000
+    }
+
+  validates :location,
+    presence: true
+end
