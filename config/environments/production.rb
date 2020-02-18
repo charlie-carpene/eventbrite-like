1cfd02a1f6ccd41a8e8437b45e643834ce93730d
Charlie Carpene <solunacisv@gmail.com>
2020-02-18 20:29:13 +0100

    add bootstrap & first try mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'blooming-waters-70444.herokuapp.com' }
+  config.action_mailer.delivery_method = :mailjet
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'events/new'
-  get 'events/create'
   root 'static_pages#index'
   get 'static_pages/secret'
   devise_for :users
   resources :events
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
